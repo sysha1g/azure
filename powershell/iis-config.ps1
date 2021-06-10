ed2f57c280d93852751d54753a6e4a7df668ba81
Syed Shah <75375572+sysha1g@users.noreply.github.com>
2021-06-10 19:49:03 +0100

    Update iis-config.ps1

--- powershell/iis-config.ps1
+++ powershell/iis-config.ps1
@@ -1,1 +1,10 @@
+Param(
+      [Parameter(Mandatory=$True,Position=1)]
+      [string]$hostname
+      )
+$iisSite = "Default Web Site"
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
+$cert = (Get-ChildItem cert:\LocalMachine\My | where-object { $_.Subject -like "*$hostname*" } | Select-Object -First 1).Thumbprint
+$guid = [guid]::NewGuid().ToString("B")
+netsh http add sslcert hostnameport="${hostname}:443" certhash=$cert certstorename=MY appid="$guid"
+New-WebBinding -name $iisSite -Protocol https -HostHeader $hostname -Port 443 -SslFlags 1
