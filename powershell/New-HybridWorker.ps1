81e85dc89ed293e24cf8bf84f74eb4c340bb7523
Syed Shah <75375572+sysha1g@users.noreply.github.com>
2021-06-22 10:14:52 +0100

    Update New-HybridWorker.ps1

--- powershell/New-HybridWorker.ps1
+++ powershell/New-HybridWorker.ps1
@@ -56,6 +56,43 @@ $agentServiceEndpoint = $AutomationInfo.Endpoint
 # Activate the Azure Automation solution in the workspace
 $null = Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $OMSResourceGroupName -WorkspaceName $WorkspaceName -IntelligencePackName "AzureAutomation" -Enabled $true
 
+$WorkspaceId = Get-AzOperationalInsightsWorkspace -ResourceGroupName $OMSResourceGroupName -Name $WorkspaceName
+$WorkspaceKey = Get-AzOperationalInsightsWorkspaceSharedKeys -ResourceGroupName $OMSResourceGroupName -Name $WorkspaceName
+
+# Check for the MMA on the machine
+try {
+
+    $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
+    
+    Write-Output "Configuring the MMA..."
+    $mma.AddCloudWorkspace($WorkspaceId, $WorkspaceKey)
+    $mma.ReloadConfiguration()
+
+} catch {
+    # Download the Microsoft monitoring agent
+    Write-Output "Downloading and installing the Microsoft Monitoring Agent..."
+
+    # Check whether or not to download the 64-bit executable or the 32-bit executable
+    if ([Environment]::Is64BitProcess) {
+        $Source = "http://download.microsoft.com/download/1/5/E/15E274B9-F9E2-42AE-86EC-AC988F7631A0/MMASetup-AMD64.exe"
+    } else {
+        $Source = "http://download.microsoft.com/download/1/5/E/15E274B9-F9E2-42AE-86EC-AC988F7631A0/MMASetup-i386.exe"
+    }
+
+    $Destination = "$env:temp\MMASetup.exe"
+
+    $null = Invoke-WebRequest -uri $Source -OutFile $Destination
+    $null = Unblock-File $Destination
+
+    # Change directory to location of the downloaded MMA
+    cd $env:temp
+
+    # Install the MMA
+    $Command = "/C:setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=$WorkspaceID" + " OPINSIGHTS_WORKSPACE_KEY=$WorkspaceKey " + " AcceptEndUserLicenseAgreement=1"
+    .\MMASetup.exe $Command
+
+}
+
 # Sleep until the MMA object has been registered
 Write-Output "Waiting for agent registration to complete..."
 
