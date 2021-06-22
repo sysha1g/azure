593f371e3a2d1ecd98730fc3ef7228ab5aa1b779
Syed Shah <75375572+sysha1g@users.noreply.github.com>
2021-06-22 12:57:09 +0100

    Update New-HybridWorker.ps1

--- powershell/New-HybridWorker.ps1
+++ powershell/New-HybridWorker.ps1
@@ -61,34 +61,46 @@ $AutomationInfo = Get-AzAutomationRegistrationInfo -ResourceGroupName $AAResourc
 $aaToken = $AutomationInfo.PrimaryKey
 $agentServiceEndpoint = $AutomationInfo.Endpoint
 
-# Check for the MMA on the machine
-try {
-
-    $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
-    
-    Write-Output "Configuring the MMA..."
-    $mma.AddCloudWorkspace($WorkspaceId, $WorkspaceKey)
-    $mma.ReloadConfiguration()
-
-} catch {
-    # Download the Microsoft monitoring agent
-    Write-Output "Downloading and installing the Microsoft Monitoring Agent..."
-
-    $Source = "https://go.microsoft.com/fwlink/?LinkId=828603"
-    $Destination = "$env:temp\MMASetup.exe"
-
-    $null = Invoke-WebRequest -uri $Source -OutFile $Destination
-    $null = Unblock-File $Destination
-
-    # Change directory to location of the downloaded MMA
-    cd $env:temp
-
-    # Install the MMA
-    $cloudType = 0
-    $Command = 'C:setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=' + $cloudType + ' OPINSIGHTS_WORKSPACE_ID="'+ $WorkspaceID +'" OPINSIGHTS_WORKSPACE_KEY="'+ $WorkspaceKey+'" AcceptEndUserLicenseAgreement=1'
-    .\MMASetup.exe $Command
-
+#Create path for the MMA agent download
+$directoryPathForMMADownload="C:\temp"
+if(!(Test-Path -path $directoryPathForMMADownload))  
+{  
+     New-Item -ItemType directory -Path $directoryPathForMMADownload
+     Write-Host "Folder path has been created successfully at: " $directoryPathForMMADownload    
 }
+else 
+{ 
+    Write-Host "The given folder path $directoryPathForMMADownload already exists"; 
+}
+
+Write-Output "Downloading MMA Agent...."
+$outputPath = $directoryPathForMMADownload + "\MMA.exe"
+
+# need to update the MMA Agent exe link for gov clouds
+Invoke-WebRequest "https://go.microsoft.com/fwlink/?LinkId=828603" -Out $outputPath
+
+Start-Sleep -s 30
+
+
+$changeDirectoryToMMALocation = "cd  $directoryPathForMMADownload"
+Invoke-Expression $changeDirectoryToMMALocation
+
+Write-Output "Extracting MMA Agent...."
+$commandToInstallMMAAgent = ".\MMA.exe /c /t:c:\windows\temp\oms"
+Invoke-Expression $commandToInstallMMAAgent
+
+Start-Sleep -s 30
+
+
+$tmpFolderOfMMA = "cd c:\windows\temp\oms"
+Invoke-Expression $tmpFolderOfMMA
+
+$cloudType = 0
+Write-Output "Connecting LA Workspace to the MMA Agent...."
+$commandToConnectoToLAWorkspace = '.\setup.exe /qn NOAPM=1 ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_AZURE_CLOUD_TYPE=' + $cloudType + ' OPINSIGHTS_WORKSPACE_ID="'+ $workspaceId +'" OPINSIGHTS_WORKSPACE_KEY="'+ $workspaceKey+'" AcceptEndUserLicenseAgreement=1'
+Invoke-Expression $commandToConnectoToLAWorkspace
+
+Start-Sleep -Seconds 60
 
 # Sleep until the MMA object has been registered
 Write-Output "Waiting for agent registration to complete..."
